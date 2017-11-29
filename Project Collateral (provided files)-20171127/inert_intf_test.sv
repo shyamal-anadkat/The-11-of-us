5bad8e27c4254bd2a56970db2b14107b4964ad45
Shyamal H Anadkat <anadkat@wisc.edu>
2017-11-29 11:09:10 -0600

    update inert_intf_test.sv

--- Project Collateral (provided files)-20171127/inert_intf_test.sv
+++ Project Collateral (provided files)-20171127/inert_intf_test.sv
@@ -1,18 +1,25 @@
-module inert_intf_test (sel, stat, strt_cal, next, cal_done, clk, rst_n);
+module inert_intf_test (LED, SS_n, SCLK, MOSI, NEXT, RST_n, clk, INT, MISO);
 
-	output logic [1:0] sel;
-	output logic stat, strt_cal;
+	output logic SS_n, SCLK, MOSI;
+	output logic [7:0] LED;
+	input NEXT, INT, RST_n, clk, MISO;
 
-	input clk, rst_n, next, cal_done;
-
-	logic SS_n, SCLK, MISO, MOSI, vld, INT;
+	logic vld, strt_cal;
 	logic [15:0] ptch, roll, yaw;
+	logic [1:0] sel;
 
 	typedef enum logic [1:0] {CAL, PTCH, ROLL, YAW} state_t;
 	state_t state, nxt_state;
 
+	reg rst_n_sync, rst_n_1;
+
+	always_ff@(posedge clk) begin
+	  rst_n_1 <= RST_n;
+	  rst_n_sync <= rst_n_1;
+	end
+
 	inert_intf intf(.clk(clk), 
-		.rst_n(rst_n), 
+		.rst_n(rst_n_sync), 
 		.strt_cal(strt_cal), 
 		.SS_n(SS_n), 
 		.SCLK(SCLK), 
@@ -26,56 +33,60 @@ module inert_intf_test (sel, stat, strt_cal, next, cal_done, clk, rst_n);
 		.yaw(yaw));
 
 //// SM implementation ////
-always_ff @(posedge clk, negedge rst_n) begin
-	if (!rst_n) begin
-		state <= CAL;
-	end else begin
-		state <= nxt_state;
-	end
+always_ff@(posedge clk or negedge rst_n_sync) begin
+  if (~rst_n_sync) begin
+    state <= CAL;
+  end else begin
+    state <= nxt_state;
+  end
 end
+
+
+assign LED = (sel == 2'b00) ? yaw[8:1] : 
+(sel == 2'b01) ? roll[8:1] : (sel == 2'b10) ? ptch[8:1] : 7'h00;
+
 
 always_comb begin
 	nxt_state = CAL;
-	stat = 1'b0;
 	strt_cal = 1'b0;
-	sel = 2'b00;
+	sel = 2'b11;
 
 	case (state)
 		CAL: begin 
 			if(cal_done) begin
 				nxt_state = PTCH;
-				sel = ptch;
+				sel = 2'b10;
 			end else begin 
 				nxt_state = CAL;
-				stat = 1;
+				sel = 2'b11;
 				strt_cal = 1;
 			end
 		end
 
 		PTCH: begin
-			if(next) begin
+			if(NEXT) begin
 				nxt_state = ROLL;
-				sel = roll;
+				sel = 2'b01;
 			end else begin
 				nxt_state = PTCH;
-				sel = ptch;
+				sel = 2'b10;
 			end
 		end
 		ROLL: begin
-			if(next) begin
+			if(NEXT) begin
 				nxt_state = YAW;
-				sel = yaw;
+				sel = 2'b00;
 			end else begin
 				nxt_state = ROLL;
-				sel = roll;
+				sel = 2'b00;
 			end
 		end
 		YAW:begin 
-			if(next) begin
+			if(NEXT) begin
 				nxt_state = PTCH;
 			end else begin
 				nxt_state = YAW;
-				sel = yaw;
+				sel = 2'b00;
 			end
 		end
 		default: begin
