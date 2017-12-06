258a51610171a42bbc1ec0d7decbcc3d07b38c7a
Eric Heinz <ejheinz@wisc.edu>
2017-12-06 11:41:37 -0600

    passing basic tests

--- Project/QuadCopter_tb.sv
+++ Project/QuadCopter_tb.sv
@@ -44,6 +44,20 @@ CommMaster iMSTR(.clk(clk), .rst_n(RST_n), .RX(TX), .TX(RX),
 			.frm_snt(cmd_sent), .resp_rdy(resp_rdy),
 			.resp(resp), .clr_resp_rdy(clr_resp_rdy));
 
+//////////////////////////////////////////////
+//          Command Opcodes                 //
+//////////////////////////////////////////////
+localparam REQ_BATT = 8'h01;
+localparam SET_PTCH = 8'h02;
+localparam SET_ROLL = 8'h03;
+localparam SET_YAW = 8'h04;
+localparam SET_THRST = 8'h05;
+localparam CALIBRATE = 8'h06;
+localparam EMER_LAND = 8'h07;
+localparam MTRS_OFF = 8'h08;
+
+localparam NO_DATA = 16'd0;
+
 initial begin
 
   ///This is where you do the real work.
@@ -51,7 +65,36 @@ initial begin
   
   //You might want to consider having several versions of this file that test several different
   //smaller things instead of having one huge test that runs forever.
+
+  // Ultimate command line sim cmd: 
+  //  vsim -gui -novopt work.QuadCopter_tb; add wave -position insertpoint sim:/QuadCopter_tb/*; run
+
   Initialize();
+
+  SendCmd(.comd(REQ_BATT), .dat(NO_DATA));
+  ChkResp(8'hC0); // fill in with what battery should be
+
+  SendCmd(.comd(SET_PTCH), .dat(16'hfa));
+  ChkPosAck;
+
+  SendCmd(.comd(SET_ROLL), .dat(16'hfa));
+  ChkPosAck;
+
+  SendCmd(.comd(SET_YAW), .dat(16'hfa));
+  ChkPosAck;
+
+  SendCmd(.comd(SET_THRST), .dat(16'hfa));
+  ChkPosAck;
+
+  SendCmd(.comd(CALIBRATE), .dat(NO_DATA));
+  ChkPosAck;
+
+  SendCmd(.comd(EMER_LAND), .dat(NO_DATA));
+  ChkPosAck;
+
+  SendCmd(.comd(MTRS_OFF), .dat(NO_DATA));
+  ChkPosAck;
+
   $display("Success!");
   $stop();
 
--- Project/tb_tasks.sv
+++ Project/tb_tasks.sv
@@ -1,5 +1,5 @@
+// Initialize task
 task Initialize();
-
 clk = 1'b0;
 RST_n = 1'b0;
 cmd_to_copter = 8'h00;
@@ -9,17 +9,41 @@ clr_resp_rdy = 1'b0;
 @(posedge clk);
 RST_n = 1'b1;
 @(posedge clk);
-
 endtask
 
-task SendCmd;
-
+// Send command task
+task SendCmd(input [7:0] comd, input [15:0] dat);
+data = dat;
+cmd_to_copter = comd;
+send_cmd = 1'b1;
+@(posedge clk);
+send_cmd = 1'b0;
+@(posedge cmd_sent);
 endtask
 
-task ChkResp;
-
+// Check response command
+task ChkResp(input [7:0] expected);
+@(posedge resp_rdy);
+if (resp != expected) begin
+	$display("inertial interface command is incorrect. Was 0x%x should be 0x%x", resp, expected);
+	$stop();
+end
+ClrRdy;
 endtask
 
+// check for ack command
 task ChkPosAck;
+@(posedge resp_rdy);
+if (resp != 8'hA5) begin
+	$display("Should have received a pos ack as response");
+	$stop();
+end
+ClrRdy;
+endtask
 
+// Knock down rdy signal
+task ClrRdy;
+clr_resp_rdy = 1'b1;
+@(posedge clk);
+clr_resp_rdy = 1'b0;
 endtask
